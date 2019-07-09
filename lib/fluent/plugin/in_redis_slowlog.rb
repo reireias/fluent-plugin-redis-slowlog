1fd5680f376b740190ae0c26aedefecd6bfa1f2e
reireias <reireias@gmail.com>
2019-07-09 21:04:07 +0900

    update for debug

--- lib/fluent/plugin/in_redis_slowlog.rb
+++ lib/fluent/plugin/in_redis_slowlog.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 require 'fluent/plugin/input'
 
 class Fluent::Redis_SlowlogInput < Fluent::Plugin::Input
   Fluent::Plugin.register_input('redis_slowlog', self)
 
   config_param :tag,      :string
-  config_param :host,     :string,  :default => nil
-  config_param :port,     :integer, :default => 6379
-  config_param :logsize,  :integer,  :default => 128
-  config_param :interval, :integer,  :default => 10
-
+  config_param :host,     :string,   default: nil
+  config_param :port,     :integer,  default: 6379
+  config_param :logsize,  :integer,  default: 128
+  config_param :interval, :integer,  default: 10
 
   def initialize
     super
@@ -23,17 +24,12 @@ class Fluent::Redis_SlowlogInput < Fluent::Plugin::Input
 
   def start
     super
-    @redis = Redis.new(
-      :host => @host, 
-      :port => @port,
-      :thread_safe => true
-    )
+    @redis = Redis.new(host: @host, port: @port, thread_safe: true)
     pong = @redis.ping
-    begin
-        unless pong == 'PONG'
-            raise "fluent-plugin-redis-slowlog: cannot connect redis"
-        end
+    unless pong == 'PONG'
+      raise 'fluent-plugin-redis-slowlog: cannot connect redis'
     end
+
     @watcher = Thread.new(&method(:watch))
   end
 
@@ -43,26 +39,24 @@ class Fluent::Redis_SlowlogInput < Fluent::Plugin::Input
   end
 
   private
+
   def watch
-    while true
+    loop do
       sleep @get_interval
-      @log_id = output( @log_id )
+      @log_id = output(@log_id)
     end
   end
 
-  def output( last_id = 0) 
-    slow_logs = []
+  def output(last_id = 0)
     slow_logs = @redis.slowlog('get', logsize)
-
+    puts slow_logs
     log_id = slow_logs[0][0]
     slow_logs.reverse.each do |log|
-      unless log[0] > last_id
-        next
-      end
+      next unless log[0] > last_id
+
       log_hash = { id: log[0], timestamp: Time.at(log[1]), exec_time: log[2], command: log[3] }
       Fluent::Engine.emit(tag, Time.now.to_i, log_hash)
     end
-    return log_id
+    log_id
   end
 end
-
