708d4488cd03f98761e31af53d755b8f36ccc244
reireias <reireias@gmail.com>
2019-07-09 20:56:17 +0900

    update test code

--- test/plugin/test_in_redis_slowlog.rb
+++ test/plugin/test_in_redis_slowlog.rb
@@ -1,24 +1,23 @@
+# frozen_string_literal: true
+
 require 'helper'
 
 class Redis_SlowlogInputTest < Test::Unit::TestCase
-
-  def setup
-    Fluent::Test.setup
-  end
-
-  CONFIG = %[
+  CONFIG = %(
       tag redis-slowlog
       host localhost
       port 6379
       logsize 128
       interval 10
-  ]
+  )
+
+  def setup
+    Fluent::Test.setup
+  end
 
   def create_driver(conf = CONFIG)
     Fluent::Test::InputTestDriver.new(Fluent::Redis_SlowlogInput).configure(conf)
   end
 
-  def test_configure
-  end
-
+  def test_configure; end
 end
